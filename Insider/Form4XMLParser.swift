ed407204d013c857bbdbd1443865cf471e08438c
John Baer <johnbaer3@gmail.com>
2021-01-24 20:27:19 -0800

    Finished parsing xml

--- Insider/Form4XMLParser.swift
+++ Insider/Form4XMLParser.swift
@@ -9,16 +9,6 @@ import UIKit
 import SwiftyXMLParser
 
 class Form4XMLParser: NSObject {
-//    var ticker: String?
-//    var companyName: String?
-//    var insiderName: String?
-//    var companyPosition: [String]?
-//    var tradeType: String?
-//    var tradePrice: Int?
-//    var tradeQty: Int?
-//    var stockCountOwnedAfter: Int?
-//    var valueOfStockInDollars: Int?
-//    var stockCountPercentChange: Int?
     
     class func requestSong(_ xmlURL: String, completionHandler: @escaping (String?, String?, Error?) -> Void) {
         let url = URL(string: xmlURL)!
@@ -42,28 +32,42 @@ class Form4XMLParser: NSObject {
             let ticker = String(xml["ownershipDocument", "issuer", "issuerTradingSymbol"].text ?? "")
             let companyName = String(xml["ownershipDocument", "issuer", "issuerName"].text ?? "")
             let insiderName = String(xml["ownershipDocument", "reportingOwner", "reportingOwnerId", "rptOwnerName"].text ?? "")
-            let isDirector = String(xml["ownershipDocument", "reportingOwner", "reportingOwnerRelationship", "isDirector"].text ?? "0")
-            let isOfficer = String(xml["ownershipDocument", "reportingOwner", "reportingOwnerRelationship", "isOfficer"].text ?? "0")
-            let isTenPercentOwner = String(xml["ownershipDocument", "reportingOwner", "reportingOwnerRelationship", "isTenpercentOwner"].text ?? "0")
-            let isOther = String(xml["ownershipDocument", "reportingOwner", "reportingOwnerRelationship", "isOther"].text ?? "0")
-            let officerTitle = String(xml["ownershipDocument", "reportingOwner", "reportingOwnerRelationship", "officerTitle"].text ?? "")
+            let isDirector = String(xml["ownershipDocument", "reportingOwner", 0, "reportingOwnerRelationship", "isDirector"].text ?? "0")
+            let isOfficer = String(xml["ownershipDocument", "reportingOwner", 0, "reportingOwnerRelationship", "isOfficer"].text ?? "0")
+            let isTenPercentOwner = String(xml["ownershipDocument", "reportingOwner", 0, "reportingOwnerRelationship", "isTenPercentOwner"].text ?? "0")
+            let isOther = String(xml["ownershipDocument", "reportingOwner", 0, "reportingOwnerRelationship", "isOther"].text ?? "0")
+            let officerTitle = String(xml["ownershipDocument", "reportingOwner", 0, "reportingOwnerRelationship", "officerTitle"].text ?? "")
             
             // TODO: Right now I can't think of a clean way to condense multiple trades in one form. So I will just parse
             //       the first trade only
-            for trade in xml["ownershipDocument", "nonDerivativeTable"]{
-                
+            let tradeInfo = String(xml["ownershipDocument", "nonDerivativeTable", "nonDerivativeTransaction", 0, "transactionAmounts", "transactionAcquiredDisposedCode", "value"].text ?? "--")
+            let tradePrice = String(xml["ownershipDocument", "nonDerivativeTable", "nonDerivativeTransaction", 0, "transactionAmounts", "transactionPricePerShare", "value"].text ?? "--")
+            let tradeQty = String(xml["ownershipDocument", "nonDerivativeTable", "nonDerivativeTransaction", 0, "transactionAmounts", "transactionShares", "value"].text ?? "")
+            let stockCountOwnedAfter = String(xml["ownershipDocument", "nonDerivativeTable", "nonDerivativeTransaction", 0, "postTransactionAmounts", "sharesOwnedFollowingTransaction", "value"].text ?? "")
+            var valueOfStockInDollars = String((Double(tradePrice) ?? 0) * (Double(tradeQty) ?? 0))
+            if valueOfStockInDollars == "0.0"{
+                valueOfStockInDollars = "--"
             }
             
+            // TODO: Calculate percentage change: Formula is value/(tradeTotalAfter-value) X 100
+//            let stockCountPercentChange = valueOfStockInDollars == "--" ?? "--" : (Double(valueOfStockInDollars) / ())
+            
+            print(ticker)
+            print(companyName)
+            print(insiderName)
+            print(isDirector)
+            print(isOfficer)
+            print(isTenPercentOwner)
+            print(isOther)
+            print(officerTitle)
+            print(tradeInfo)
+            print(tradePrice) //fail
+            print(tradeQty)
+            print(stockCountOwnedAfter)
+            print(valueOfStockInDollars) //fail
             
             
-            
-            // access XML Text
-
-//            if let text = xml["ResultSet", "Result", "Hit", 0, "Name"].text {
-//                print(text)
-//            }
-            
-            completionHandler("delegate.song", "delegate.artist", nil) //parser.parserError)
+            completionHandler("delegate.song", "delegate.artist", nil)
         }
         task.resume()
     }
